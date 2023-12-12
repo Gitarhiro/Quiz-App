1431e13ac6b55e3e5057e495bfd387e6aadf5bf3
Szymon Bedeniczuk <benio4089@gmail.com>
2023-12-12 18:38:24 +0100

    added solution test

--- quiz-project/src/solution/solution.service.spec.ts
+++ quiz-project/src/solution/solution.service.spec.ts
@@ -1,8 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SolutionService } from './solution.service';
+import { async } from 'rxjs';
 
 describe('SolutionService', () => {
   let service: SolutionService;
+
+  const mockAnswerRepository ={
+    SolveTheQuiz: jest.fn(dto=>dto),
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,4 +20,15 @@ describe('SolutionService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should get a result from the test',async()=>{
+    expect(await service.solveTheQuiz({
+      quiz_id:1,
+      answers:[{
+        id: 1,
+        contents: 'Warszawa',
+        sortOrder: null,
+      }] 
+    })).toEqual({score:1})
+  })
 });
